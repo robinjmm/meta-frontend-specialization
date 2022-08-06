586ef06e595fd86ad43433114f8e79e3ef22e337
J.M. Robin <johnmichaelrobin@gmail.com>
2022-08-06 12:48:38 +0800

    Complete Javascript Operators exercise

---
+++ 02-programming-with-javascript/02-operators.js
@@ -0,0 +1,33 @@
+// Task #1:
+let score = 8;
+console.log("Mid-level skills: " + (score > 0 && score < 10));
+
+// Task #2:
+let timeRemaining = 0;
+let energy = 0;
+console.log("Game Over: " + (timeRemaining === 0 || energy === 0));
+
+// Task #3:
+let num1 = 2;
+let num2 = 5;
+let test1 = num1 % 2;
+let test2 = num2 % 2;
+let result1 = test1 === 0;
+let result2 = test2 === 0;
+console.log("Is", num1, "an even number?", result1);
+console.log("is", num2, "an even number", result2);
+
+// Task #4:
+console.log(5 + 10);
+
+// Task #5:
+let now = "Now in ";
+let three = 3;
+let d = "D!";
+console.log(now + three + d);
+
+// Task #6:
+let counter = 0;
+counter += 5;
+counter += 3;
+console.log(counter);
