6eb10ad2e571cb420e99057fdd9d11e93f8a455d
J.M. Robin <johnmichaelrobin@gmail.com>
2022-08-06 13:05:55 +0800

    Complete Javascript Loops exercise

---
+++ 02-programming-with-javascript/04-loops.js
@@ -0,0 +1,50 @@
+// Task #1:
+console.log("Task #1:");
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
+console.log("Counting completed!");
+
+// Task #2:
+console.log("=========================");
+console.log("Task #2:");
+for (let i = 5; i > 0; i--) {
+    console.log(i);
+}
+console.log("Countdown finished!");
+
+// Task #3:
+console.log("=========================");
+console.log("Task #3:")
+let i = 0;
+
+while (i < 5) {
+    i++;
+    console.log(i);
+}
+
+console.log("Counting completed");
+
+// Task #4:
+console.log("=========================");
+console.log("Task #4:")
+
+let j = 5;
+
+while (j > 0) {
+    console.log(j);
+    j--;
+}
+
+console.log("Countdown finished!");
+
+// Task #5:
+console.log("=========================");
+console.log("Task #5:");
+
+let n = 2018;
+
+while (n <= 2022) {
+    console.log(n);
+    n++;
+}
