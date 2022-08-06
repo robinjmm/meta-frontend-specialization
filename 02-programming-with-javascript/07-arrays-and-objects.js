23203e79343a64a3d6776d6c2f3be19222ae8ed8
J.M. Robin <johnmichaelrobin@gmail.com>
2022-08-06 21:45:24 +0800

    Complete Creating Arrays and Objects exercise

---
+++ 02-programming-with-javascript/07-arrays-and-objects.js
@@ -0,0 +1,12 @@
+// Task #1:
+let clothes = [];
+clothes.push("hoodie", "t-shirt", "shorts", "khaki pants", "cap");
+clothes.pop();
+clothes.push("handkerchief");
+console.log(clothes[2]);
+
+// Task #2:
+let favCar = {};
+favCar.color = "black";
+favCar.convertible = false;
+console.log(favCar);
