ef5267c07778a7ea0a1a522fa5efef693e1b9483
J.M. Robin <johnmichaelrobin@gmail.com>
2022-08-06 12:25:35 +0800

    Complete Declaring Variables exercise

---
+++ 02-programming-with-javascript/01-declaring-variables.js
@@ -0,0 +1,13 @@
+let petDog = "Rex";
+let petCat = "Pepper";
+console.log(petDog);
+console.log(petCat);
+console.log("My pet dog's name is: " + petDog);
+console.log("My pet cat's name is: " + petCat);
+
+let catSound = "purr";
+let dogSound = "woof";
+console.log(petDog + " says " + dogSound);
+console.log(petCat + " says " + catSound);
+catSound = "meow";
+console.log(petCat + " now says " + catSound);
