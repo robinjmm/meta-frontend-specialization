09e9565151fea6af3269e6de50a4b8861130f2b1
J.M. Robin <johnmichaelrobin@gmail.com>
2022-08-08 13:28:04 +0800

    Complete Error Prevention exercise

---
+++ 02-programming-with-javascript/08-error-prevention.js
@@ -0,0 +1,17 @@
+function addTwoNums(a, b) {
+    try {
+        if (typeof a !== "number") {
+            throw new ReferenceError("The first number is not a number");
+        } else if (typeof b !== "number") {
+            throw new ReferenceError("The second number is not a number");
+        } else {
+            console.log(a + b);
+        }
+    } catch (err) {
+        console.log("Error", err);
+    }
+}
+
+addTwoNums(5, "5");
+
+console.log("It still works!");
